1b1cc046248c95974a84fc84fa28e910591f71ff
yiakonale <yiakonale@hotmail.com>
2024-03-23 16:33:00 -0400

    set16

--- set16.m
+++ set16.m
@@ -1,19 +1,24 @@
+%Ioannis Papaspyridis
 clc; %clear the command window
 clear; %clear all variables
 
+%set16
+
 NumberOfTurns=200; %Number of turns of the solenoid
-Rin=1.5; %inner radius of toroid
-Rout=2.5; %outer radius of toroid
+Rin=1.5e-2; %inner radius of toroid
+Rout=2.5e-2; %outer radius of toroid
 Ravg=(Rin+Rout)/2;
 r=(Rout-Rin)/2; %internal radius of toroid
 
-Phimin=0; %coordinate of the lowest point on the solenoid
-Phimax=2*pi; %coordinate of the highest point on the solenoid
+Phimin=0; %coordinate of the lowest angle on the solenoid
+Phimax=2*pi; %coordinate of the highest angle on the solenoid
 t_min=0; %lowest value of the curve parameter t
-t_max=NumberOfTurns*2.0*pi; % for every turn we have an angle increment of 2*pi
-NumberOfSegments=100; %we divide the toroid into this number of segments
+t_max=NumberOfTurns*2.0*pi; %for every turn we have an angle increment of 2*pi
+
+NumberOfSegments=NumberOfTurns*360; %we divide the toroid into this number of segments (360 degrees per turn)
 t_values=linspace(t_min,t_max,(NumberOfSegments+1))'; %these are the values of the parameter t
-phi_values=Phimin+((Phimax-Phimin)/(t_max-t_min))*(t_values-t_min);
+phi_values=Phimin+((Phimax-Phimin)/(t_max-t_min))*(t_values-t_min); %phi values increment with t values
+
 x_values=(Ravg+r*cos(t_values)).*cos(phi_values);
 y_values=(Ravg+r*cos(t_values)).*sin(phi_values);
 z_values=r*sin(t_values);
@@ -22,10 +27,10 @@ I=5; %value of current
 NumberOfXPlottingPoints=20; %number of plotting points along the x axis
 NumberOfYPlottingPoints=20; %number of plotting points along the y axis
 
-PlotXmin=-4.0; %lowest x value on the plot plane
-PlotXmax=4.0; %maximum x value on the plot plane
-PlotYmin=-4.0; %lowest y value on the plot plane
-PlotYmax=4.0; %maximum y value on the plot plane
+PlotXmin=-4.0e-2; %lowest x value on the plot plane
+PlotXmax=4.0e-2; %maximum x value on the plot plane
+PlotYmin=-4.0e-2; %lowest y value on the plot plane
+PlotYmax=4.0e-2; %maximum y value on the plot plane
 PlotZ=0; %all points on observation plane have zero z coordinate
 
 PlotStepX=(PlotXmax-PlotXmin)/(NumberOfXPlottingPoints-1);%plotting step in the x direction
